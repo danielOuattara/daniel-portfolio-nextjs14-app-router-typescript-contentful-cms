73fa7342518003f3f6ebd27c70336917924e5628
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-01 15:04:43 +0200

    logic in Submenu

--- src/components/Submenu.tsx
+++ src/components/Submenu.tsx
@@ -1,6 +1,50 @@
-// import { useState, useRef, useEffect } from "react";
+"use client";
+
+import { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 
-export default function Submenu() {
-  return <h2>Submenu</h2>;
+export default function Submenu({
+  isSubMenuOpen,
+  subMenuLocation,
+  subMenuPageToShow,
+  setIsSubMenuOpen,
+}: TypeSubmenu) {
+  const [columns, setColumns] = useState("");
+  const subMenuContainerRef = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    subMenuPageToShow.subLinks.length <= 3
+      ? setColumns("col-2")
+      : setColumns("col-3");
+
+    if (subMenuContainerRef.current) {
+      subMenuContainerRef.current.style.left = `${subMenuLocation.subMenuCenterPosition}px`;
+      subMenuContainerRef.current.style.top = `${subMenuLocation.subMenuTopPosition}px`;
+    }
+  }, [subMenuLocation, subMenuPageToShow.subLinks]);
+
+  const hideSubMenu = () => setIsSubMenuOpen(false);
+
+  return (
+    <aside
+      className={isSubMenuOpen ? "submenu show" : "submenu"}
+      ref={subMenuContainerRef}
+      onMouseLeave={hideSubMenu}
+    >
+      <div className={` nav-subLinks submenu-center ${columns} `}>
+        {subMenuPageToShow.subLinks.map((subLink, index) => {
+          return (
+            <Link
+              key={index}
+              href={subLink.url}
+              className="link-btn"
+              onClick={() => setIsSubMenuOpen(false)}
+            >
+              {subLink.page}
+            </Link>
+          );
+        })}
+      </div>
+    </aside>
+  );
 }
