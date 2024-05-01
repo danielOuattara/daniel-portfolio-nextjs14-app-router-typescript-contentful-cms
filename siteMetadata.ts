7e6d774436adc37f63bdf5f2fa8b9f3bd3487495
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-01 16:50:50 +0200

    about page

---
+++ siteMetadata.ts
@@ -0,0 +1,31 @@
+export const siteMetadata = {
+  title: "Fullstack Portfolio",
+  siteUrl: "https://daniel-portfolio-gatsby-ts-contentful.netlify.app",
+  description:
+    "Portfolio Gatsby & Contentful powered website where I present myself and show my skills on different kind of projects and using a wide range of effective web design technologies.",
+  image: "/homeImage.png",
+  linkedInUsername: "daniel-b-ouattara-677146ab",
+  twitterUsername: "@danielOuattara",
+  name: "Daniel Ouattara",
+  date: "Sept. 2023",
+  aboutMe: {
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi commodiaperiam officiis nostrum maxime reprehenderit, totam eaque corporisrepudiandae soluta quos neque omnis dolore quibusdam aliquam,consectetur excepturi error suscipit.",
+    stack: [
+      "html",
+      "javascript",
+      "typescript",
+      "node",
+      "deno",
+      "express",
+      "postgresql",
+      "mongodb",
+      "css",
+      "sass",
+      "react",
+      "vue",
+      "gatsby",
+      "next",
+    ],
+    image: "./src/assets/images/hero2.svg",
+  },
+};
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,23 @@
+import { Title } from "@/components";
+import { siteMetadata } from "../../../siteMetadata";
+import Image from "next/image";
+import HeroImage from "./../../assets/images/hero2.svg";
+
 export default function AboutPage() {
-  return <h1>About Page</h1>;
+  return (
+    <section className="about-page">
+      <div className="section-center about-center">
+        <Image src={HeroImage} alt="portfolio" className="hero-img" />
+        <article className="about-text">
+          <Title title={"about"} />
+          <p>{siteMetadata.aboutMe.text}</p>
+          <div className="about-stack">
+            {siteMetadata.aboutMe.stack.map((item) => (
+              <span key={item}>{item}</span>
+            ))}
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 }
