208d12536c4d8b7a297ccddc4b0ffa2b1c14f89d
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-01 14:35:28 +0200

    add Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,5 +4,33 @@ import { page_links } from "../constants";
 import Link from "next/link";
 
 export default function Navbar() {
-  return <h2>Navbar</h2>;
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <Link href="/">
+            <img
+              src={fullStackLogo as never as string}
+              alt="fullStack Web Developer"
+              className="logo"
+            />
+          </Link>
+          <button type="button" className="toggle-btn">
+            <FaAlignJustify />
+          </button>
+        </div>
+        <div className="nav-links">
+          {page_links.map((link) => (
+            <Link
+              key={link.id}
+              href={link.url}
+              className={link.subLinks ? "link-btn has-subLinks" : "link-btn"}
+            >
+              {link.page}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
 }
