92ae1363584b0866267b186cc89558dc6d334ee3
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-01 16:51:11 +0200

    update not-found.tsx logic

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,39 @@
+import Link from "next/link";
+import React from "react";
+
 export default function NotFound() {
-  return <h1>Not Found</h1>;
+  return (
+    <main className="error-page">
+      <div className="error-container">
+        <h1>404</h1>
+        <h3>page not found 😔</h3>
+        <p style={paragraphStyles}>
+          Sorry, we couldn’t find what you were looking for.
+          <br />
+          {process.env.NODE_ENV === "development" ? (
+            <>
+              <br />
+              Try creating a page in <code style={codeStyles}>src/pages/</code>.
+              <br />
+            </>
+          ) : null}
+          <br />
+        </p>
+        <Link href="/" className="btn">
+          back safely to home
+        </Link>
+      </div>
+    </main>
+  );
 }
+
+const paragraphStyles: React.CSSProperties = {
+  marginBottom: 48,
+};
+const codeStyles: React.CSSProperties = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4,
+};
