4b36ca5c0e88db1c43d8195353130344a3708236
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-01 11:15:31 +0200

    fetch featured projects on Home Page + log them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Hero, Projects, Services } from "@/components";
 import type { Metadata } from "next";
+import { fetchFeaturedProjects } from "@/contentful/portfolioProjects";
+import { draftMode } from "next/headers";
 
 export const metadata: Metadata = {
   title: "Home page",
@@ -7,7 +9,14 @@ export const metadata: Metadata = {
     "Home page for Daniel portfolio where skills for fullstack wed development are presented as projects",
 };
 
-export default function Home() {
+export default async function Home() {
+  const featuredProjects = await fetchFeaturedProjects({
+    preview: draftMode().isEnabled,
+  });
+
+  console.log("featuredProjects.length = ", featuredProjects.length);
+  console.log("featuredProjects = ", featuredProjects);
+
   return (
     <main>
       <h1>Home page</h1>
@@ -17,3 +26,34 @@ export default function Home() {
     </main>
   );
 }
+
+/* 
+
+
+
+export default async function Home() {
+  // Fetch blog posts using the content preview if draft mode is enabled:
+
+  const blogPosts = await fetchBlogPosts({ preview: draftMode().isEnabled });
+  return (
+    <main className="p-[64vw]">
+      <div className="prose">
+        <h1>My Contentful Blog</h1>
+        <ul>
+          {blogPosts.map((singlePost) => (
+            <li key={singlePost.slug}>
+              <Link href={`/${singlePost.slug}`}>{singlePost.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+
+
+
+
+
+*/
