fc75f018d90d72a54f16448ddf76cde5256eaf64
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-01 15:04:33 +0200

    Logic in Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,25 +2,29 @@ import { page_links, social_links } from "../constants";
 import Link from "next/link";
 import { FaTimes } from "react-icons/fa";
 
-export default function Sidebar() {
+export default function Sidebar({ isSidebarOpen, toggleSidebar }: TypeSideBar) {
   return (
-    <aside className={"sidebar show-sidebar"}>
-      <button className="close-btn" type="button">
+    <aside className={isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}>
+      <button className="close-btn" type="button" onClick={toggleSidebar}>
         <FaTimes />
       </button>
       <div className="side-container">
         {/* page links & sub links */}
-        <ul className="sidebar-links">
+        <ul className={isSidebarOpen ? "sidebar-links" : ""}>
           {page_links.map((link) => {
             return (
               <li key={link.id}>
-                <Link href={link.url}>{link.page}</Link>
+                <Link href={link.url} onClick={toggleSidebar}>
+                  {link.page}
+                </Link>
                 {link.subLinks && (
                   <ul className="sidebar-subLinks">
                     {link.subLinks.map((subLink) => {
                       return (
                         <li key={subLink.id}>
-                          <Link href={subLink.url}>{subLink.page}</Link>
+                          <Link href={subLink.url} onClick={toggleSidebar}>
+                            {subLink.page}
+                          </Link>
                         </li>
                       );
                     })}
@@ -32,7 +36,7 @@ export default function Sidebar() {
         </ul>
 
         {/* social links */}
-        <ul className={"social-links sidebar-icons"}>
+        <ul className={isSidebarOpen ? "social-links sidebar-icons" : ""}>
           {social_links.map((link) => (
             <li key={link.id}>
               <a href={link.url} className="social-link">
