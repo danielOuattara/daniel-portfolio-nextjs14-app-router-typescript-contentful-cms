794f7beac42df569108f3320f8166076d895e2de
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-01 13:38:25 +0200

    added Services

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,5 +2,19 @@ import { Title } from "./index";
 import { services } from "@/constants";
 
 export default function Services() {
-  return <h2>Services</h2>;
+  return (
+    <section className="section bg-grey">
+      <Title title="Services" />
+      <div className="section-center services-center">
+        {services.map((service) => (
+          <article key={service.id} className="service">
+            {service.icon}
+            <h4>{service.title}</h4>
+            <div className="underline"></div>
+            <p>{service.text}</p>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
 }
