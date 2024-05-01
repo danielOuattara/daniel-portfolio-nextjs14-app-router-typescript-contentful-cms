cea1821e1750faa5e906bd1e58545c9a847aa1c8
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-01 15:42:08 +0200

    add Footer logic

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,37 @@
 import socialLinks from "@/constants/social_links";
 
 export default function Footer() {
-  return <h2>footer</h2>;
+  return (
+    <footer className="footer">
+      <div>
+        <div className="footer-links social-links">
+          {socialLinks.map((link) => (
+            <a
+              href={link.url}
+              key={link.id}
+              className="social-link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+        <h4>
+          copyright &copy; 2019 - {new Date().getFullYear()}{" "}
+          <span>Web & Mobile </span> all right reserved
+        </h4>
+        <p>Powered using : Nextjs14 - Typescript - Contentful</p>
+        <p>
+          <a
+            href="https://github.com/danielOuattara/daniel-portfolio-nextjs14-app-router-typescript-contentful-cms"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github Source
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 }
