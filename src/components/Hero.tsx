04cdbab89f2e061bb77f05dd0cabbe6341098f40
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-01 13:36:58 +0200

    added Hero: OK, TODO: correct width

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,43 @@
 import Link from "next/link";
 import { social_links } from "@/constants";
 import Image from "next/image";
+import HeroImg from "./../assets/images/hero2.svg";
 
 export default function Hero() {
-  return <h2>Hero</h2>;
+  return (
+    <header className="hero">
+      <section className="section-center hero-center">
+        <article className="hero-info">
+          <div className="hero-card">
+            <h1>I am Daniel</h1>
+            <div className="underline"></div>
+            <h4> Fullstack Developer: Web & Mobile</h4>
+            <Link href={"/contact"} className="btn">
+              Contact
+            </Link>
+            <div className="social-links">
+              {social_links.map((link) => (
+                <a
+                  href={link.url}
+                  key={link.id}
+                  className="social-link"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {link.icon}
+                </a>
+              ))}
+            </div>
+          </div>
+        </article>
+        <Image
+          src={HeroImg}
+          alt="portfolio"
+          className="hero-img"
+          // width={300}
+          // height={300}
+        />
+      </section>
+    </header>
+  );
 }
