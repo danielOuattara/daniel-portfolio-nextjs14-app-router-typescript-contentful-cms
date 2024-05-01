7f4c938b1e871bf8487ac240642a2204e485604b
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-01 15:04:16 +0200

    Logic in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,20 +2,57 @@ import fullStackLogo from "./../assets/images/full_stack_logo.svg";
 import { FaAlignJustify } from "react-icons/fa";
 import { page_links } from "../constants";
 import Link from "next/link";
+import Image from "next/image";
 
-export default function Navbar() {
+export default function Navbar(props: TypeNavbar) {
+  //-----
+  const displaySubMenu = (event: React.MouseEvent<HTMLElement>) => {
+    const targetElement = event.target as HTMLElement; // Type assertion
+
+    const menuDOMRect: DOMRect = targetElement.getBoundingClientRect();
+
+    props.setSubMenuLocation({
+      subMenuCenterPosition: (menuDOMRect.left + menuDOMRect.right) / 2,
+      subMenuTopPosition: menuDOMRect.bottom,
+    });
+
+    const menuPageName: string = event.currentTarget.textContent || "";
+
+    const menuPageToShow = page_links.find(
+      (item) => item.page === menuPageName,
+    );
+
+    if (menuPageToShow && menuPageToShow.subLinks) {
+      props.setSubMenuPageToShow(menuPageToShow);
+    }
+
+    props.setIsSubMenuOpen(true);
+  };
+
+  //----
+  const hideSubMenu = (event: React.MouseEvent<HTMLElement>) => {
+    const targetElement = event.target as HTMLElement; // Type assertion
+    if (!targetElement.classList.contains("has-subLinks")) {
+      props.setIsSubMenuOpen(false);
+    }
+  };
+
   return (
-    <nav className="navbar">
+    <nav className="navbar" onMouseOver={(event) => hideSubMenu(event)}>
       <div className="nav-center">
         <div className="nav-header">
           <Link href="/">
-            <img
+            <Image
               src={fullStackLogo as never as string}
               alt="fullStack Web Developer"
               className="logo"
             />
           </Link>
-          <button type="button" className="toggle-btn">
+          <button
+            type="button"
+            className="toggle-btn"
+            onClick={props.toggleSidebar}
+          >
             <FaAlignJustify />
           </button>
         </div>
@@ -25,6 +62,8 @@ export default function Navbar() {
               key={link.id}
               href={link.url}
               className={link.subLinks ? "link-btn has-subLinks" : "link-btn"}
+              onMouseOver={(event) => link.subLinks && displaySubMenu(event)}
+              onClick={() => props.setIsSubMenuOpen(false)}
             >
               {link.page}
             </Link>
