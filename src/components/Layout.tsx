d1d27f60a4ebb72870b9630c9cf3117d12bbdbc7
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-01 15:03:13 +0200

    add logic for props in Navbar, Sidebar and Submenu

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,39 @@
+"use client";
+import { useState } from "react";
 import { Navbar, Sidebar, Footer, Submenu } from "./index";
 import "@/assets/css/main.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+  const [subMenuLocation, setSubMenuLocation] = useState({
+    subMenuCenterPosition: 0,
+    subMenuTopPosition: 0,
+  });
+  const [subMenuPageToShow, setSubMenuPageToShow] =
+    useState<TypeSubMenuPageToShow>({
+      id: 0,
+      url: "",
+      page: "",
+      subLinks: [],
+    });
+
+  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
   return (
     <>
-      <Navbar />
-      <Sidebar />
-      <Submenu />
+      <Navbar
+        toggleSidebar={toggleSidebar}
+        setIsSubMenuOpen={setIsSubMenuOpen}
+        setSubMenuLocation={setSubMenuLocation}
+        setSubMenuPageToShow={setSubMenuPageToShow}
+      />
+      <Sidebar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
+      <Submenu
+        isSubMenuOpen={isSubMenuOpen}
+        subMenuLocation={subMenuLocation}
+        subMenuPageToShow={subMenuPageToShow}
+        setIsSubMenuOpen={setIsSubMenuOpen}
+      />
       {children}
       <Footer />
     </>
