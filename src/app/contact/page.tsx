d66132a83ba3e7c610dfe0ed5f3301655de21822
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-06 18:38:15 +0200

    add contact page: TODO: check if activated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,49 @@
 export default function ContactPage() {
-  return <h1>Contact Page</h1>;
+  return (
+    <section className="contact-page">
+      <article className="contact-form">
+        <h3>stay in touch ?</h3>
+        <form
+          method="POST"
+          action="https://formspree.io/f/mdorpabo"
+          className="form contact-form"
+        >
+          <div className="form-group">
+            <label htmlFor="name">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="name"
+                className="form-control"
+              />
+            </label>
+            <label htmlFor="email">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="email"
+                className="form-control"
+              />
+            </label>
+            <label htmlFor="message">
+              <textarea
+                name="message"
+                id="message"
+                rows={7}
+                className="form-control"
+                placeholder="your message"
+              ></textarea>
+            </label>
+          </div>
+          <button type="submit" className="submit-btn btn">
+            submit
+          </button>
+        </form>
+      </article>
+    </section>
+  );
 }
+
+// export const Head = () => <Seo title={"Contacts"} image="/contact.png" />;
