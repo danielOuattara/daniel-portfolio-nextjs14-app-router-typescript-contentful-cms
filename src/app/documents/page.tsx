bb9cd8834c6dac6cfcdde3ca3cefbffa9ee86c7e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-06 20:56:52 +0200

    render all documents in documents root page

--- src/app/documents/page.tsx
+++ src/app/documents/page.tsx
@@ -1,3 +1,30 @@
-export default function DocumentsPage() {
-  return <h1>Documents Page</h1>;
+import { Documents } from "@/components";
+
+import type { Metadata } from "next";
+import { fetchDocuments } from "@/contentful/portfolioDocuments";
+import { draftMode } from "next/headers";
+
+export const metadata: Metadata = {
+  title: "Projects | Portfolio ",
+  description:
+    "Projects page for Daniel portfolio where one can find fullstack, frontend, backend and mobile projects, with link to hosted CDN",
+};
+
+export default async function DocumentsPage() {
+  const allDocuments = await fetchDocuments({
+    preview: draftMode().isEnabled,
+  });
+
+  return (
+    <main>
+      <section className="projects-page">
+        <Documents
+          documents={allDocuments}
+          showLinkToDiploma={false}
+          showItemNumber={false}
+          title={"All Documents"}
+        />
+      </section>
+    </main>
+  );
 }
