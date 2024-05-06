dfa77c1a3ea1b65f62f7977957366763c5a03805
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-06 18:41:43 +0200

    update logic fro fetching project by Category + single project

--- src/app/projects/[...slug]/page.tsx
+++ src/app/projects/[...slug]/page.tsx
@@ -1,36 +1,66 @@
-import { fetchProjectsByCategory } from "@/contentful/portfolioProjects";
+/* 
+slug[0] = category name
+slug[1] = project name slugged
+
+*/
+import Link from "next/link";
+import Image from "next/image";
+import {
+  fetchProjects,
+  // fetchProjectsByCategory,
+  fetchSingleProject,
+} from "@/contentful/portfolioProjects";
 import { draftMode } from "next/headers";
 import { Projects } from "@/components";
 
 type Params = {
   params: {
-    slug: string[] | undefined;
+    slug: string[];
   };
 };
 
 export default async function ParamsPage({ params: { slug } }: Params) {
-  console.log(slug);
-
-  if (slug?.length === 1) {
-    const allProjectsByCategory = await fetchProjectsByCategory({
+  if (slug.length === 1) {
+    const allProjectsByCategory = await fetchProjects({
       preview: draftMode().isEnabled,
-      category: slug[0],
+      category: slug[0] as "backend" | "frontend" | "fullstack" | "mobile",
     });
 
     return (
       <main>
         <section className="projects-page">
-          <Projects title="all projects" projects={allProjectsByCategory} />
+          <Projects
+            title={`${slug[0]} projects`}
+            projects={allProjectsByCategory}
+          />
         </section>
       </main>
     );
   }
 
-  if (slug?.length === 2) {
+  if (slug.length === 2) {
+    const singleProject = await fetchSingleProject({
+      preview: draftMode().isEnabled,
+      slug: slug[1],
+    });
+
     return (
-      <h2>
-        viewing page for {slug[0]} and {slug[1]}
-      </h2>
+      <main className="project-template-page">
+        <h2>{singleProject.title}</h2>
+        <p>{singleProject.description}</p>
+        <a href={singleProject.url_website} target="_blank" rel="noreferrer">
+          <Image
+            src={`https://${singleProject.featured_image?.src}`}
+            alt={singleProject.title}
+            width={singleProject.featured_image?.width}
+            height={singleProject.featured_image?.height}
+            className="project-img"
+          />
+        </a>
+        <Link href="/projects" className="btn">
+          back to projects
+        </Link>
+      </main>
     );
   }
 }
