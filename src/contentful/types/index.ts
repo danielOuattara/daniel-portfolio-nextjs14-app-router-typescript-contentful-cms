ace5ece27964978be50b65c7003c852106a64c29
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-06-18 21:09:36 +0200

    types updated

--- src/contentful/types/index.ts
+++ src/contentful/types/index.ts
@@ -1,7 +1,11 @@
-export type { TypeAuthor, TypeAuthorFields, TypeAuthorSkeleton } from "./TypeAuthor";
-export type { TypeBlogPostMaxSchmitt, TypeBlogPostMaxSchmittFields, TypeBlogPostMaxSchmittSkeleton } from "./TypeBlogPostMaxSchmitt";
-export type { TypePortfolioDocuments, TypePortfolioDocumentsFields, TypePortfolioDocumentsSkeleton } from "./TypePortfolioDocuments";
-export type { TypePortfolioProjects, TypePortfolioProjectsFields, TypePortfolioProjectsSkeleton } from "./TypePortfolioProjects";
-export type { TypePost, TypePostFields, TypePostSkeleton } from "./TypePost";
-export type { TypeProjectsContentfulJohnSmilga, TypeProjectsContentfulJohnSmilgaFields, TypeProjectsContentfulJohnSmilgaSkeleton } from "./TypeProjectsContentfulJohnSmilga";
-export type { TypeSimpleRecipesGatsbyJohnSmilga, TypeSimpleRecipesGatsbyJohnSmilgaFields, TypeSimpleRecipesGatsbyJohnSmilgaSkeleton } from "./TypeSimpleRecipesGatsbyJohnSmilga";
+export type {
+  TypePortfolioDocuments,
+  TypePortfolioDocumentsFields,
+  TypePortfolioDocumentsSkeleton,
+} from "./TypePortfolioDocuments";
+
+export type {
+  TypePortfolioProjects,
+  TypePortfolioProjectsFields,
+  TypePortfolioProjectsSkeleton,
+} from "./TypePortfolioProjects";
