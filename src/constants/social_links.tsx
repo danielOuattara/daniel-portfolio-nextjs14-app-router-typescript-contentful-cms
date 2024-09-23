a3e5525f2f4f82caeebf4b03fbaedfb6c496742a
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-09-23 23:46:46 +0200

    clean social links

--- src/constants/social_links.tsx
+++ src/constants/social_links.tsx
@@ -11,26 +11,26 @@ import {
 import { SiMaildotru } from "react-icons/si";
 
 const social_links = [
-  {
-    id: 1,
-    icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-    url: "http://www.linkedin.com/in/daniel-b-ouattara-677146ab",
-  },
+  // {
+  //   id: 1,
+  //   icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
+  //   url: "http://www.linkedin.com/in/daniel-b-ouattara-677146ab",
+  // },
   {
     id: 2,
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
     url: "http://www.linkedin.com/in/daniel-b-ouattara-677146ab",
   },
-  {
-    id: 3,
-    icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-    url: "http://www.linkedin.com/in/daniel-b-ouattara-677146ab",
-  },
-  {
-    id: 4,
-    icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-    url: "http://www.linkedin.com/in/daniel-b-ouattara-677146ab",
-  },
+  // {
+  //   id: 3,
+  //   icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
+  //   url: "http://www.linkedin.com/in/daniel-b-ouattara-677146ab",
+  // },
+  // {
+  //   id: 4,
+  //   icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
+  //   url: "http://www.linkedin.com/in/daniel-b-ouattara-677146ab",
+  // },
   {
     id: 5,
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
