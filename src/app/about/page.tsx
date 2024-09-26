3660743b15f2c7c2c755535417c91655f334aceb
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-09-26 10:03:22 +0200

    About page image responsive

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,22 @@
 import { Title } from "@/components";
 import { siteMetadata } from "../../../siteMetadata";
 import Image from "next/image";
-import HeroImage from "./../../assets/images/hero2.svg";
+import HeroImage from "./../../assets/images/about-me-image.svg";
 
 export default function AboutPage() {
   return (
     <section className="about-page">
       <div className="section-center about-center">
-        <Image src={HeroImage} alt="portfolio" className="hero-img" />
+        <Image
+          src={HeroImage}
+          alt="about me"
+          className="hero-img"
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+            // objectFit: "cover",
+          }}
+        />
         <article className="about-text">
           <Title title={"about"} />
           <p>{siteMetadata.aboutMe.text}</p>
