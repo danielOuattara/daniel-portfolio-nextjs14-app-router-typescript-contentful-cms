6c2c6a68f1e062e2c73b7a77dc4c66deea7be6cc
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-03 15:39:11 +0200

    update logic for single project render

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import { IProject } from "@/contentful/portfolioProjects";
+import { FaGithubSquare } from "react-icons/fa";
+import { HiAtSymbol } from "react-icons/hi";
 
 export default function Project({
   singleProject,
@@ -9,24 +11,43 @@ export default function Project({
 }) {
   return (
     <main className="project-template-page">
-      <h2>{singleProject.title}</h2>
+      <h2>{singleProject.category} project</h2>
+      <h3>{singleProject.title}</h3>
+      <div>
+        <div className="project-template-img-container">
+          <Image
+            src={`https:${singleProject.featured_image?.src}`}
+            alt={singleProject.title}
+            width={singleProject.featured_image?.width}
+            height={singleProject.featured_image?.height}
+            className="project-template-img"
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+            }}
+          />
+        </div>
+
+        <div className="project-stack">
+          {singleProject.technologies.slice(0, 4).map((techno) => (
+            <span key={techno}>{techno}</span>
+          ))}
+        </div>
+      </div>
+
       <p>{singleProject.description}</p>
-      <a href={singleProject.url_website} target="_blank" rel="noreferrer">
-        <Image
-          src={`https:${singleProject.featured_image?.src}`}
-          alt={singleProject.title}
-          width={singleProject.featured_image?.width}
-          height={singleProject.featured_image?.height}
-          className="project-img"
-          style={{
-            maxWidth: "100%",
-            height: "auto",
-          }}
-        />
-      </a>
-      <Link href="/projects" className="btn">
-        back to projects
-      </Link>
+
+      <div className="project-links">
+        <Link href={`/projects/${singleProject.category}`} className="btn">
+          &lt; back
+        </Link>
+        <a href={singleProject.url_github} target="_blank" rel="noreferrer">
+          <FaGithubSquare className="project-icon" />
+        </a>
+        <a href={singleProject.url_website} target="_blank" rel="noreferrer">
+          <HiAtSymbol className="project-icon" />
+        </a>
+      </div>
     </main>
   );
 }
