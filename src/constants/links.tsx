1d40ff140258b91e5cbba704281a5062482b20cb
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-06 23:59:14 +0200

    services: update nav links

--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -4,6 +4,7 @@ import {
   FaServer,
   FaMobileAlt,
   FaEnvelopeOpenText,
+  FaTools,
 } from "react-icons/fa";
 import { GrProjects } from "react-icons/gr";
 import { FaSquareWebAwesome } from "react-icons/fa6";
@@ -13,7 +14,6 @@ import { GiDiploma } from "react-icons/gi";
 import { PiCertificate } from "react-icons/pi";
 import { TbFileCv } from "react-icons/tb";
 import { FiPackage } from "react-icons/fi";
-import { MdMarkEmailRead } from "react-icons/md";
 
 const page_links = [
   {
@@ -30,30 +30,36 @@ const page_links = [
   },
   {
     id: 3,
+    url: "/services",
+    page: "services",
+    icon: <FaTools />,
+  },
+  {
+    id: 4,
     url: "/projects",
     page: "projects",
     icon: <GrProjects />,
     subLinks: [
       {
-        id: "3-1",
+        id: "4-1",
         url: "/projects/backend",
         page: "backend",
         icon: <FaServer />,
       },
       {
-        id: "3-2",
+        id: "4-2",
         url: "/projects/frontend",
         page: "frontend",
         icon: <FaSquareWebAwesome />,
       },
       {
-        id: "3-3",
+        id: "4-3",
         url: "/projects/fullstack",
         page: "fullstack",
         icon: <HiOutlineDesktopComputer />,
       },
       {
-        id: "3-4",
+        id: "4-4",
         url: "/projects/mobile",
         page: "mobile",
         icon: <FaMobileAlt />,
@@ -61,31 +67,31 @@ const page_links = [
     ],
   },
   {
-    id: 4,
+    id: 5,
     url: "/documents",
     page: "documents",
     icon: <IoDocumentText />,
     subLinks: [
       {
-        id: "4-1",
+        id: "5-1",
         url: "/documents/certificates",
         page: "certificates",
         icon: <PiCertificate />,
       },
       {
-        id: "4-2",
+        id: "5-2",
         url: "/documents/diplomas",
         page: "diplomas",
         icon: <GiDiploma />,
       },
       {
-        id: "4-3",
+        id: "5-3",
         url: "/documents/cv",
         page: "curriculum",
         icon: <TbFileCv />,
       },
       {
-        id: "4-4",
+        id: "5-4",
         url: "/documents/packages",
         page: "packages",
         icon: <FiPackage />,
@@ -93,7 +99,7 @@ const page_links = [
     ],
   },
   {
-    id: 5,
+    id: 6,
     page: "contact",
     url: "/contact",
     icon: <FaEnvelopeOpenText />,
