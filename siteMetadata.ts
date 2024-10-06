7ec2d90a33caa4578acaee55d1ace6bb23315cd5
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-07 00:32:41 +0200

    update siteMetadata: aboutMe

--- siteMetadata.ts
+++ siteMetadata.ts
@@ -9,7 +9,13 @@ export const siteMetadata = {
   name: "Daniel Ouattara",
   date: "Sept. 2023",
   aboutMe: {
-    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi commodiaperiam officiis nostrum maxime reprehenderit, totam eaque corporisrepudiandae soluta quos neque omnis dolore quibusdam aliquam,consectetur excepturi error suscipit.",
+    text: `I am a full-stack developer with over 12 years of experience building dynamic, secure, and scalable web applications. My expertise spans from crafting responsive, intuitive user interfaces to developing robust back-end systems. I’m proficient in frameworks like React, Next.js, Express, and Nest.js, which allow me to deliver high-quality applications for both web and mobile platforms.
+
+On the front-end, I focus on creating fast, accessible, and engaging user experiences, implementing responsive designs that work seamlessly across devices. Using tools like React and Next.js, I ensure efficient performance through strategies like lazy loading and code splitting.
+
+On the back-end, I design secure architectures using frameworks such as Express and Nest.js. I prioritize security, scalability, and clean code in everything I build, whether it's a simple REST API or a complex, data-driven system. I also have experience with both SQL and NoSQL databases, ensuring secure and optimized data management.
+
+Collaboration is key for me, and I enjoy working with teams to bring ideas to life, whether it's for startups or enterprise projects. My goal is always to deliver high-quality, user-centric solutions that meet business objectives.`,
     stack: [
       "html",
       "javascript",
