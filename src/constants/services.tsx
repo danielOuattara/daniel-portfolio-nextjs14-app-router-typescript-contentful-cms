826657a049d1890c243de31f69212cbd243af3af
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-06 21:33:20 +0200

    update info on services section

--- src/constants/services.tsx
+++ src/constants/services.tsx
@@ -1,23 +1,45 @@
 import React from "react";
-import { FaCode, FaSketch, FaAndroid } from "react-icons/fa";
+import { FaCode, FaAndroid, FaServer } from "react-icons/fa";
+import { BsDatabaseFillCheck } from "react-icons/bs";
+import { MdHealthAndSafety } from "react-icons/md";
+import { GrSecure } from "react-icons/gr";
+
 const services = [
   {
     id: 1,
-    icon: <FaCode className="service-icon" />,
-    title: "frontend web & mobile development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    icon: (
+      <>
+        <FaCode className="service-icon" />
+        &nbsp;
+        <FaAndroid className="service-icon" />
+      </>
+    ),
+    title: "Frontend Web & Mobile Development",
+    text: `I craft responsive and dynamic user interfaces for both web and mobile platforms, ensuring seamless performance, accessibility, and user experience across all devices.`,
   },
   {
     id: 2,
-    icon: <FaSketch className="service-icon" />,
-    title: "backend robust & secured architecture",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    icon: (
+      <>
+        <GrSecure className="service-icon" />
+        &nbsp;
+        <FaServer className="service-icon" />
+      </>
+    ),
+    title: "Backend Robust & Secure Architecture",
+    text: `Specializing in building scalable, secure, and efficient backend systems, I design architecture that supports high-traffic applications while ensuring data integrity and performance optimization.`,
   },
   {
     id: 3,
-    icon: <FaAndroid className="service-icon" />,
-    title: "database handle",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    icon: (
+      <>
+        <MdHealthAndSafety className="service-icon" />
+        &nbsp;
+        <BsDatabaseFillCheck className="service-icon" />
+      </>
+    ),
+    title: "Database Management",
+    text: `Proficient in managing complex databases, I ensure high availability, data security, and optimized queries for performance, handling large datasets with precision and care.`,
   },
 ];
 
