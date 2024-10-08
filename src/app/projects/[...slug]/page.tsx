e1a3416aabf3d029c8570b31e04445761e0f1d6c
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-08 21:38:18 +0200

    logic for og & twitter card in products

--- src/app/projects/[...slug]/page.tsx
+++ src/app/projects/[...slug]/page.tsx
@@ -8,16 +8,18 @@ import {
 } from "@/contentful/portfolioProjects";
 import { Project, ProjectCardList } from "@/components";
 import { notFound } from "next/navigation";
+import type { Metadata, ResolvingMetadata } from "next";
+
+//----
 
 type Params = {
   params: {
     slug: string[];
   };
+  searchParams?: { [key: string]: string | string[] | undefined };
 };
 
 type CategorySlugType = "backend" | "frontend" | "fullstack" | "mobile";
-
-//----
 
 export async function generateStaticParams(): Promise<{ slug: string[] }[]> {
   const categories: CategorySlugType[] = [
@@ -43,9 +45,119 @@ export async function generateStaticParams(): Promise<{ slug: string[] }[]> {
 
 //----
 
+export async function generateMetadata(
+  { params, searchParams }: Params,
+  parent: ResolvingMetadata,
+): Promise<Metadata | undefined> {
+  const isDraftMode = draftMode().isEnabled;
+  const slugArray = Array.isArray(params?.slug) ? params.slug : [params?.slug];
+  const [categorySlug, projectNameSlug] = slugArray;
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  try {
+    if (!categorySlug) {
+      return undefined;
+    }
+    if (categorySlug && projectNameSlug) {
+      // Fetch & render a single project from a category
+      const singleProject = await fetchSingleProject({
+        preview: isDraftMode,
+        slug: projectNameSlug,
+      });
+
+      if (!singleProject) {
+        return undefined;
+      }
+
+      return {
+        openGraph: {
+          title: singleProject.title,
+          description: `${singleProject.description}`,
+          url: `https://daniel-portfolio-next-ts-contentful.vercel.app/${singleProject.category}/${singleProject.slug}`,
+          siteName: "daniel's portfolio",
+          images: [
+            {
+              url: `https:${singleProject.featured_image?.src}`,
+              width: 1200,
+              height: 630,
+              alt: `https:${singleProject.title}`,
+            },
+            ...previousImages,
+          ],
+          locale: "en_US",
+          type: "website",
+        },
+        twitter: {
+          card: "summary_large_image", // 'summary', 'summary_large_image', etc.
+          title: singleProject.title,
+          description: `${singleProject.description}`,
+          site: "daniel's portfolio",
+          images: [
+            {
+              url: `https:${singleProject.featured_image?.src}`,
+              width: 1200,
+              height: 630,
+              alt: singleProject.title,
+            },
+          ], // Twitter-specific image
+        },
+      };
+    }
+
+    // Fetch & render all projects by category
+    const projectsByCategory = await fetchProjects({
+      preview: isDraftMode,
+      category: categorySlug as CategorySlugType,
+    });
+
+    if (projectsByCategory.length === 0) {
+      return undefined;
+    }
+
+    return {
+      openGraph: {
+        title: `${categorySlug} projects`,
+        description: `List of all ${projectsByCategory} website`,
+        url: `https://daniel-portfolio-next-ts-contentful.vercel.app/${projectsByCategory}-projects`,
+        siteName: "daniel's portfolio",
+        images: [
+          {
+            url: `https://daniel-portfolio-next-ts-contentful.vercel.app/${projectsByCategory}-projects`,
+            width: 1200,
+            height: 630,
+            alt: `https:${projectsByCategory}`,
+          },
+          ...previousImages,
+        ],
+        locale: "en_US",
+        type: "website",
+      },
+      twitter: {
+        card: "summary_large_image", // 'summary', 'summary_large_image', etc.
+        title: `${categorySlug} projects`,
+        description: `List of all ${projectsByCategory} website`,
+        site: "daniel's portfolio",
+        images: [
+          {
+            url: `https://daniel-portfolio-next-ts-contentful.vercel.app/${projectsByCategory}`,
+            width: 1200,
+            height: 630,
+            alt: `https:${projectsByCategory}`,
+          },
+        ], // Twitter-specific image
+      },
+    };
+  } catch (error) {
+    return;
+  }
+}
+
+//----
+
 export default async function ProjectSlugPage({ params }: Params) {
   const isDraftMode = draftMode().isEnabled;
-
   const slugArray = Array.isArray(params?.slug) ? params.slug : [params?.slug];
   const [categorySlug, projectNameSlug] = slugArray;
 
@@ -90,7 +202,6 @@ export default async function ProjectSlugPage({ params }: Params) {
       </main>
     );
   } catch (error) {
-    console.error("An error occurred:", error);
     notFound();
   }
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -9,6 +9,35 @@ export const metadata: Metadata = {
   title: "Projects page | Portfolio ",
   description:
     "Projects page for Daniel portfolio where one can find fullstack, frontend, backend and mobile projects, with link to hosted CDN",
+
+  openGraph: {
+    title: "Daniel's Portfolio, FullStack Developer",
+    description:
+      "Projects page for Daniel portfolio where skills for fullstack web development are presented as projects. In this page one can find all websites, representing projects. Frontend, Backend, FullStack, Mobile",
+    url: "https://daniel-portfolio-next-ts-contentful.vercel.app/projects",
+    siteName: "daniel's portfolio",
+    images: [
+      {
+        url: "https://daniel-portfolio-next-ts-contentful.vercel.app/projects.png", // Image for Open Graph previews
+        width: 1200,
+        height: 630,
+        alt: "image showing website home page",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image", // 'summary', 'summary_large_image', etc.
+    title: "Daniel's Portfolio, FullStack Developer",
+    description:
+      "Projects page for Daniel portfolio where skills for fullstack web development are presented as projects. In this page one can find all websites, representing projects. Frontend, Backend, FullStack, Mobile",
+    // images: ["https://yourwebsite.com/twitter-image.jpg"], // Twitter-specific image
+    images: [
+      "https://daniel-portfolio-next-ts-contentful.vercel.app/all-projects.png",
+    ], // Twitter-specific image
+  },
 };
 
 //------------------------
