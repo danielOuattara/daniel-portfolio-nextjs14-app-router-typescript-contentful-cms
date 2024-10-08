b99a569d1aa326785c82e42e49a3c25ab62382e3
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-09 00:00:02 +0200

    update Title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,7 @@
 export default function Title({ title }: { title: string }) {
   return (
     <div className="section-title" id={`${title}`}>
-      <h2>{title || "Section Title"}</h2>
+      <h1>{title || "Section Title"}</h1>
       <div className="underline"></div>
     </div>
   );
