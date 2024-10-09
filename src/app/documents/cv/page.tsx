3ef3e2ee508d138f8262576448099034a106da91
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-09 03:04:37 +0200

    CV page

---
+++ src/app/documents/cv/page.tsx
@@ -0,0 +1,47 @@
+import { Title } from "@/components";
+import Link from "next/link";
+import { FaCloudDownloadAlt } from "react-icons/fa";
+import { IoIosNavigate } from "react-icons/io";
+
+export default function CurriculumPage() {
+  return (
+    <section className="section section-center documents">
+      <Title title="cv" />
+
+      <div className="cv-container" style={{}}>
+        <Link
+          href="https://daniel-portfolio-next-ts-contentful.vercel.app/cvCurriculum_Vitae_Daniel_Ouattara.pdf"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="download the curriculum"
+          download
+        >
+          <span>download cv</span>
+          <span>
+            <FaCloudDownloadAlt />
+          </span>
+        </Link>
+
+        <Link
+          href="https://cv-daniel-ouattara-fullstack.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="download the curriculum"
+          download
+        >
+          <span>navigate to cv</span>
+          <span>
+            <IoIosNavigate />
+          </span>
+        </Link>
+      </div>
+      <div className="section-center documents-center cv-iframe-container">
+        <br />
+        <iframe
+          src="https://cv-daniel-ouattara-fullstack.vercel.app/"
+          className="responsive-iframe"
+        ></iframe>
+      </div>
+    </section>
+  );
+}
