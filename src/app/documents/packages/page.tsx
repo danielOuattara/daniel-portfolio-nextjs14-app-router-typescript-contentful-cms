b3c867ffe5d186836dfc8517a9aa5720fd672e10
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-10 13:42:26 +0200

    add message for packages section of documents

---
+++ src/app/documents/packages/page.tsx
@@ -0,0 +1,17 @@
+import { Title } from "@/components";
+import Link from "next/link";
+import { FaCloudDownloadAlt } from "react-icons/fa";
+import { IoIosNavigate } from "react-icons/io";
+
+export default function CurriculumPage() {
+  return (
+    <section className="section section-center ">
+      <Title title="packages list" />
+      <p>
+        List of packages I use as a developer, in my full-stack, back-end and
+        front-end projects. <br /> Coming soon !
+      </p>
+      <p>Thank you</p>
+    </section>
+  );
+}
